899556ab53915c84174e07248bbbd18d130db3a0
sitchatt <sitabjachatterjee158@gmail.com>
2023-08-03 22:02:57 +0530

    Added explorer.sh

--- Challenges/Day_2/explorer.sh
+++ Challenges/Day_2/explorer.sh
@@ -4,42 +4,31 @@
 echo "Welcome to the Interactive File and Directory Explorer!"
 echo
 echo "Files and Directories in the Current Path:"
-ls -lh | awk ' 
-    function human_readable(size) {
-    suffixes = "KMGT"
+ls -l | awk '
+function human_readable(size) {
+    suffixes = "BKMGT"
     for (i = 1; size >= 1024 && i <= length(suffixes); i++) {
         size /= 1024
     }
-
-    # Calculate the decimal part
-    decimal = size - int(size)
-
-    # Round up if the decimal part is greater than 0.5
-    if (decimal > 0.5) {
-        size = int(size) + 1
-    } else {
-        size = int(size)
-    }
-
-    return size substr(suffixes, i, 1)
+    return sprintf("%.2f", size) substr(suffixes, i, 1)
 }
+NR > 1 {
+    size = human_readable($5);
+    print "- " $9 " (" size ")"
+}'
 
-    
-    {print "- " $9 " (" human_readable($5) "B)"}'
-'
 echo
 
-# Loop for Part 2: Character Counting
+# Part 2: Character Counting
 while true; do
     read -p "Enter a line of text (Press Enter without text to exit): " input
-    if [[ -z "$input" ]]; then
+    if [ -z "$input" ]; then
         echo "Exiting the Interactive Explorer. Goodbye!"
         break
     else
         char_count=$(echo -n "$input" | wc -m)
         echo "Character Count: $char_count"
-        word_count=$(echo -n "$input" | wc -w)
+        word_count=$(echo -n "$input" | wc -m)
         echo "Word Count: $word_count"
-
     fi
 done
